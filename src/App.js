feaa4b4c7ec186a1cabe60da6f1d4ec41d276b49
Mohan Raj <mohanraj.pachaiyappan@news.co.uk>
2021-11-29 18:53:16 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	return (
+		<div data-test="app">
+			<h1 data-test="counter-text">
+				The counter is <span data-test="count">{count}</span>
+			</h1>
+			<button data-test="inc-button" onClick={() => setCount(count + 1)}>
+				Increment counter
+			</button>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<App/>', () => {
+	const setup = () => shallow(<App />);
+
+	const findByTestAttr = (wrapper, value) =>
+		wrapper.find(`[data-test="${value}"]`);
+
+	test('renders non empty component without crashing', () => {
+		const wrapper = setup();
+
+		const appComp = findByTestAttr(wrapper, 'app');
+		expect(appComp.length).toBe(1);
+	});
+
+	test('renders counter display', () => {
+		const wrapper = setup();
+		const counterText = findByTestAttr(wrapper, 'counter-text');
+		expect(counterText.length).toBe(1);
+	});
+	test('renders increment button', () => {
+		const wrapper = setup();
+		const button = findByTestAttr(wrapper, 'inc-button');
+		expect(button.length).toBe(1);
+	});
+	test('counter display starts with 0', () => {
+		const wrapper = setup();
+		const count = findByTestAttr(wrapper, 'count');
+		expect(count.text()).toBe('0');
+	});
+	test('clicking increment button increments counter display', () => {});
 });
